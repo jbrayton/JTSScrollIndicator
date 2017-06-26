fa7e8a3519a6a6f457be5880472e17ff26b1ae84
John Brayton <jbrayton@goldenhillsoftware.com>
2017-06-26 15:17:42 -0400

    Carthage-ready Xcode framework project.

---
+++ JTSScrollIndicator/JTSScrollIndicator.h
@@ -0,0 +1,19 @@
+//
+//  JTSScrollIndicator.h
+//  JTSScrollIndicator
+//
+//  Created by John Brayton on 6/26/17.
+//  Copyright © 2017 John Brayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTSScrollIndicator.
+FOUNDATION_EXPORT double JTSScrollIndicatorVersionNumber;
+
+//! Project version string for JTSScrollIndicator.
+FOUNDATION_EXPORT const unsigned char JTSScrollIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTSScrollIndicator/PublicHeader.h>
+
+
